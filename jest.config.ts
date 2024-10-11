7eeece711281e75c95b190c75e3430b72f63f1ef
Wandinho Oliveira <inovaprog@gmail.com>
2024-10-11 01:35:17 -0300

    chore: adjust jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,13 @@ module.exports = {
   preset: "ts-jest", // Use ts-jest preset
   testEnvironment: "node", // Set the test environment to Node.js
   moduleFileExtensions: ["ts", "js", "json", "node"], // Include file extensions
-  transform: {
-    "^.+\\.ts$": "ts-jest", // Transform TypeScript files
-  },
-  // Optionally, if you're using ES modules
   extensionsToTreatAsEsm: [".ts"],
-  globals: {
-    "ts-jest": {
-      useESM: true, // Enable ESM for ts-jest
-    },
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
   },
 };
